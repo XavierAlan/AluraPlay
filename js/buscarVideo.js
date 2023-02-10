1cbde453de35c1221e4743e9a43b7c3b15091e30
XavierAlan <100752217+XavierAlan@users.noreply.github.com>
2023-02-10 17:17:53 -0300

    Projeto concluido
    Sem o npm json-server

---
+++ js/buscarVideo.js
@@ -0,0 +1,26 @@
+import { conectaAPI } from "./conectaAPI.js";
+import constroiCard from "./mostrarVideos.js";
+
+async function buscarVideo(evento) {
+    evento.preventDefault();
+
+    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
+    const busca = await conectaAPI.buscaVideos(dadosDePesquisa)
+
+    const lista = document.querySelector("[data-lista]");
+
+    while(lista.firstChild){
+        lista.removeChild(lista.firstChild);
+    }
+
+    busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+
+    if(busca.length == 0){
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não existem videos com esse termo</h2>`;
+    }
+}
+
+
+const botaoPesquisa = document.querySelector("[data-botao-pesquisa]");
+
+botaoPesquisa.addEventListener("click", evento => buscarVideo(evento))
